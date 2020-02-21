2c69c428613716fcf3ef09c2ebd56beb3b5bcdbc
jeremyndeby <jeremy.ndeby@gmail.com>
2020-02-21 12:02:28 +0100

    nouvelle version geomap

---
+++ venv/Scripts/pip3-script.py
@@ -0,0 +1,12 @@
+#!"C:\Users\jerem\Google Drive\Mes Documents\Travail\Projects\Toulouse_Apt_Rental_Price\venv\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==19.0.3','console_scripts','pip3'
+__requires__ = 'pip==19.0.3'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('pip==19.0.3', 'console_scripts', 'pip3')()
+    )
